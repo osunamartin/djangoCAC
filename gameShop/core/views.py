0a67bfd1fd416c6e1f7c19e79e325ade33132f96
fabiang78 <cursosfabiangomez@gmail.com>
2023-09-19 19:00:16 -0300

    carga de archivos

---
+++ gameShop/core/views.py
@@ -0,0 +1,33 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from datetime import datetime
+
+# Create your views here.
+def index(request):
+  context = {
+    'nombre_usuario':'Juan',
+    'fecha':datetime.now(),
+    'es_cliente': False,
+  }
+  
+  return render(request, "core/index.html", context)
+
+def producto_lista(request):
+  context = {
+    'nombre_usuario': 'Juan',
+    'hay_stock': True,
+  }
+  return render(request, 'core/productos_lista.html', context)
+
+def producto_detalle(request, nombre_producto):
+  return HttpResponse(
+    f"""
+    <h1>Bienvenido {nombre_producto} </h1>
+    <p>Detalle del producto</p>
+    """
+  )
+
+def producto_hay_stock(request, hay_stock):
+  return HttpResponse(f'¿Hay stock de este producto?: {hay_stock}') 
+
+
