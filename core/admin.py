a2d62abb329a71826d07a6a3c6fd2b5a66256fca
Leandro Melchiori <leandromelchiori@gmail.com>
2023-11-16 05:53:46 -0300

    manytomany editable admin v2

--- core/admin.py
+++ core/admin.py
@@ -25,29 +25,38 @@ sitio_admin.register(Categoria)
 sitio_admin.register(Wishlist)
 """
 
+
 class CategoriaProductoForm(forms.ModelForm):
     class Meta:
         model = Categoria_Producto
         fields = '__all__'
 
+    def clean(self):
+        cleaned_data = super().clean()
+        producto = cleaned_data.get('producto')
+        categoria = cleaned_data.get('categoria')
+
+        if producto and categoria:
+            # Verificar si el producto ya está asociado a la categoría
+            if Categoria_Producto.objects.filter(producto=producto, categoria=categoria).exists():
+                raise forms.ValidationError("El producto ya está en esta categoría")
 
 class CategoriaProductoInline(admin.TabularInline):
     model = Categoria.productos.through
-    extra = 1
     form = CategoriaProductoForm
-
+    extra = 1
 
 @admin.register(Categoria)
 class CategoriaAdmin(admin.ModelAdmin):
     inlines = [CategoriaProductoInline]
 
-
 @admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'descripcion', 'proveedor', 'precio', 'stock')
-    list_editable = ('descripcion', 'proveedor', 'precio', 'stock')
+    list_editable = ('descripcion', 'proveedor', 'stock')
     list_display_links = ('nombre',)
-    search_fields = ['proveedor']
+    search_fields = ['nombre']
 
-admin.site.register(Wishlist)
 admin.site.register(Proveedor)
+admin.site.register(Wishlist)
+admin.site.register(Categoria_Producto)
